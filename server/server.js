ccda52add1869e62bfc99ca2c31c8ec6a453073b
dharmendra <webteam2@dfcc.co.in>
2026-05-07 17:54:43 +0530

    fix cors frontend url

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://your-frontend.vercel.app",
+      "https://hacker-news-app-git-main-dharmusisri245s-projects.vercel.app",
     ],
     credentials: true,
   })
